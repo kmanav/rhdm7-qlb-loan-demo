e14281f9b6897a41339524d61764bf7141011153
Manav Kumar <mankumar@redhat.com>
2018-05-28 09:16:43 -0400

    Update config.js

--- support/openshift/config/config.js
+++ support/openshift/config/config.js
@@ -2,8 +2,6 @@
 
 var ENV = {
     'kieserver_host' : 'http://rhdm7-qlb-loan-kieserver-rhdm7-qlb-loan-developer.192.168.64.29.nip.io',
-    'kieserver_port' : '8080',
-    'kieserver_contextroot' : '',
     'kieserver_containerId' : 'loan-application_1.0',
     'kieserver_user' : 'kieserver',
     'kieserver_password' : 'kieserver1!'
